347175084e325e1eae3f5afed7ff4d96c42a1171
Pythyu <antoine.zellmeyer@datadoghq.com>
2023-06-26 11:07:28 +0200

    fix(circleCI): ansible-role linter

--- ci_test/collection-structure-setup.sh
+++ ci_test/collection-structure-setup.sh
@@ -3,7 +3,7 @@
 pip install ansible-lint==6.17 galaxy-importer
 
 # lint the ansible-role alone
-ansible-lint -v --profile=production --exclude=galaxy.yml --exclude=meta/ --exclude=ci_test/ --exclude=manual_tests/
+ansible-lint -v --profile=production --exclude=galaxy.yml --exclude=meta/ --exclude=ci_test/ --exclude=manual_tests/ --exclude=.circleci/
 
 mkdir ansible_collections
 cd ansible_collections
